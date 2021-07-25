55c95117a984fdf53712e2d5be454c845791b638
unknown <azizleyla01gmail.com>
2021-07-25 20:01:19 +0400

    finish project

--- app.js
+++ app.js
@@ -1,155 +1,162 @@
-const headerTags = document.querySelector('.header__tags')
-const container = document.querySelector('.container')
-const headerContent = document.querySelector('.header__content');
+const headerTags = document.querySelector(".header__tags");
+const container = document.querySelector(".container");
+const headerContent = document.querySelector(".header__content");
+const clearBtn = document.querySelector('#clear-btn')
 
 let jobAnouncements = [
   {
     id: 1,
-    title: 'Senior Frontend Developer',
-    coverPhoto: 'photosnap.svg',
-    published: '1d ago',
-    shift: 'Full Time',
-    place: 'USA only',
-    tags: ['Frontend', "Senior", 'HTML', 'CSS', 'JavaScript'],
+    title: "Senior Frontend Developer",
+    coverPhoto: "photosnap.svg",
+    published: "1d ago",
+    shift: "Full Time",
+    place: "USA only",
+    tags: ["Frontend", "Senior", "HTML", "CSS", "JavaScript"],
     isFeatured: true,
     isNew: true,
-    companyName: 'Photosnap'
+    companyName: "Photosnap",
   },
   {
     id: 2,
-    title: 'Full Stack Developer',
-    coverPhoto: 'manage.svg',
-    published: '1d ago',
-    shift: 'Part time',
-    place: 'Remote',
-    tags: ['Fullstack', 'Midweight', 'Python', 'React'],
+    title: "Full Stack Developer",
+    coverPhoto: "manage.svg",
+    published: "1d ago",
+    shift: "Part time",
+    place: "Remote",
+    tags: ["Fullstack", "Midweight", "Python", "React"],
     isFeatured: false,
     isNew: true,
-    companyName: 'Manage'
+    companyName: "Manage",
   },
   {
     id: 3,
-    title: 'Account',
-    coverPhoto: 'account.svg',
-    published: '2d ago',
-    shift: 'Part Time',
-    place: 'USA only',
-    tags: ['Frontend', 'Junior', 'React', "Sass", "Javascript"],
+    title: "Account",
+    coverPhoto: "account.svg",
+    published: "2d ago",
+    shift: "Part Time",
+    place: "USA only",
+    tags: ["Frontend", "Junior", "React", "Sass", "Javascript"],
     isFeatured: false,
     isNew: true,
-    companyName: 'Account'
+    companyName: "Account",
   },
 
   {
     id: 4,
-    title: 'Junior Frontend Developer',
-    coverPhoto: 'myhome.svg',
-    published: '2d ago',
-    shift: 'Contract',
-    place: 'USA only',
-    tags: ['Frontend', 'Junior', 'Css', "Javascript"],
+    title: "Junior Frontend Developer",
+    coverPhoto: "myhome.svg",
+    published: "2d ago",
+    shift: "Contract",
+    place: "USA only",
+    tags: ["Frontend", "Junior", "Css", "Javascript"],
     isFeatured: false,
     isNew: false,
-    companyName: 'MyHome',
+    companyName: "MyHome",
   },
   {
     id: 5,
-    title: 'Loop Studios',
-    coverPhoto: 'loop-studios.svg',
-    published: '1w ago',
-    shift: 'Contract',
-    place: 'USA only',
-    tags: ['Fullstack', 'Midweight', "Javascript", "Sass", "Ruby"],
+    title: "Loop Studios",
+    coverPhoto: "loop-studios.svg",
+    published: "1w ago",
+    shift: "Contract",
+    place: "USA only",
+    tags: ["Fullstack", "Midweight", "Javascript", "Sass", "Ruby"],
     isFeatured: false,
     isNew: false,
-    companyName: 'Software Engineer',
+    companyName: "Software Engineer",
   },
 
   {
     id: 6,
-    title: 'Junior Backend Developer',
-    coverPhoto: 'faceit.svg',
-    published: '2w ago',
-    shift: 'Full Time',
-    place: 'UK only',
-    tags: ['Backend', 'Junior', 'Ruby', 'Ror'],
+    title: "Junior Backend Developer",
+    coverPhoto: "faceit.svg",
+    published: "2w ago",
+    shift: "Full Time",
+    place: "UK only",
+    tags: ["Backend", "Junior", "Ruby", "Ror"],
     isFeatured: false,
     isNew: false,
-    companyName: 'FaceIt'
+    companyName: "FaceIt",
   },
   {
     id: 7,
-    title: 'Junior Developer',
-    coverPhoto: 'shortly.svg',
-    published: '2w ago',
-    shift: 'Full Time',
-    place: 'Worldwide',
-    tags: ['Frontend', 'Junior', 'HTML', "Sass", 'JavaScript'],
+    title: "Junior Developer",
+    coverPhoto: "shortly.svg",
+    published: "2w ago",
+    shift: "Full Time",
+    place: "Worldwide",
+    tags: ["Frontend", "Junior", "HTML", "Sass", "JavaScript"],
     isFeatured: false,
     isNew: false,
-    companyName: 'Shortly'
+    companyName: "Shortly",
   },
 
   {
     id: 8,
-    title: 'Junior Frontend Developer',
-    coverPhoto: 'insure.svg',
-    published: '2w ago',
-    shift: 'Full Time',
-    place: 'USA only',
-    tags: ['Frontend', 'Junior', 'Vue', 'JavaScript', "Sass"],
+    title: "Junior Frontend Developer",
+    coverPhoto: "insure.svg",
+    published: "2w ago",
+    shift: "Full Time",
+    place: "USA only",
+    tags: ["Frontend", "Junior", "Vue", "JavaScript", "Sass"],
     isFeatured: false,
     isNew: false,
-    companyName: 'Insure'
+    companyName: "Insure",
   },
   {
     id: 9,
-    title: 'Full Stack Engineer',
-    coverPhoto: 'eyecam-co.svg',
-    published: '3w ago',
-    shift: 'Full Time',
-    place: 'Worlwide',
-    tags: ['Fullstack', 'Midweight', 'Javascript', 'Django', "Python"],
+    title: "Full Stack Engineer",
+    coverPhoto: "eyecam-co.svg",
+    published: "3w ago",
+    shift: "Full Time",
+    place: "Worlwide",
+    tags: ["Fullstack", "Midweight", "Javascript", "Django", "Python"],
     isFeatured: false,
     isNew: false,
-    companyName: 'Eyecam Co.'
+    companyName: "Eyecam Co.",
   },
   {
     id: 10,
-    title: 'Full Stack Engineer',
-    coverPhoto: 'the-air-filter-company.svg',
-    published: '1ma ago',
-    shift: 'Full Time',
-    place: 'Worlwide',
-    tags: ['Frontend', 'Junior', 'React', "Sass", 'JavaScript'],
+    title: "Full Stack Engineer",
+    coverPhoto: "the-air-filter-company.svg",
+    published: "1ma ago",
+    shift: "Full Time",
+    place: "Worlwide",
+    tags: ["Frontend", "Junior", "React", "Sass", "JavaScript"],
     isFeatured: false,
     isNew: false,
-    companyName: 'The Air Filter Company'
+    companyName: "The Air Filter Company",
   },
 ];
 
-let selectedTags = []
-
-
-
-// jobAnouncements.forEach((jobsAnouncement, index) => {
-//   jobsAnouncement.id = index + 1;
-// })
-
+let selectedTags = [];
 
 function updateUI(jobAnouncements) {
-  container.innerHTML = ""
+  container.innerHTML = "";
 
-  jobAnouncements.forEach(announcement => {
-
-    const { title, coverPhoto, published, shift, place, tags, isFeatured, isNew, companyName, id } = announcement;
+  jobAnouncements.forEach((announcement) => {
+    const {
+      title,
+      coverPhoto,
+      published,
+      shift,
+      place,
+      tags,
+      isFeatured,
+      isNew,
+      companyName,
+      id,
+    } = announcement;
     let taghtml = "";
-    tags.forEach(tag => {
-      taghtml = taghtml + `<span class="job-card__tag">${tag}</span>`
-    })
+    tags.forEach((tag) => {
+      taghtml = taghtml + `<span class="job-card__tag">${tag}</span>`;
+    });
 
-    const jobCardElement = document.createElement('div');
-    jobCardElement.setAttribute('class', `job-card ${isNew && isFeatured ? "job-card--left" : ""}`)
+    const jobCardElement = document.createElement("div");
+    jobCardElement.setAttribute(
+      "class",
+      `job-card ${isNew && isFeatured ? "job-card--left" : ""}`
+    );
     jobCardElement.innerHTML = `
 
     <div class="job-card__cover">
@@ -158,8 +165,14 @@ function updateUI(jobAnouncements) {
     <div class="job-card__desrcription">
       <div class="job-card__description-top">
         <span class="job-card__company-name">${companyName}</span>
-    ${isNew ? `<span class="job-card__badge job-card__badge--new">New</span>` : ""}
-    ${isFeatured ? ` <span class="job-card__badge job-card__badge--featured" > Featured</span >` : ""}
+    ${isNew
+        ? `<span class="job-card__badge job-card__badge--new">New</span>`
+        : ""
+      }
+    ${isFeatured
+        ? ` <span class="job-card__badge job-card__badge--featured" > Featured</span >`
+        : ""
+      }
       </div>
 
       <p class="job-card__title">${title}</p>
@@ -173,148 +186,80 @@ function updateUI(jobAnouncements) {
         ${taghtml}
       </div>
 
-      `
+      `;
 
-    container.appendChild(jobCardElement)
-
-  })
-
-  // container.querySelectorAll('.job-card__tag').forEach(tag => {
-
-
-  //   tag.addEventListener('click', function (e) {
-  //     headerContent.classList.add('show')
-  //     const div = document.createElement('div');
-  //     div.setAttribute('class', "job-card__tags1")
-  //     div.innerHTML = `
-  //     <span class="job-card__tag">${tag.textContent}<i class ="fas fa-times close">X</i></span>
-  //     `
-  //     if (!headerTags.textContent.includes(tag.textContent)) {
-  //       headerTags.append(div)
-  //     }
-  //     let filteredData = jobAnouncements.filter(jobsAnouncement => jobsAnouncement.tags.includes(tag.textContent));
-  //     updateUI(filteredData)
-  //   })
-
-  // })
-
+    container.appendChild(jobCardElement);
+  });
 }
 
-updateUI(jobAnouncements)
-
-container.addEventListener('click', function (e) {
-
-  const targetElement = e.target
-  if (targetElement.classList.contains('job-card__tag')) {
-
-    if (selectedTags.includes(targetElement.textContent)) {
-      const filteredSelectedTags = selectedTags.filter((item) => {
-        return item !== targetElement.textContent
-      })
-      selectedTags = filteredSelectedTags;
-
+container.addEventListener("click", function (e) {
+  const clickedElement = e.target;
+  if (clickedElement.classList.contains("job-card__tag")) {
+    if (selectedTags.includes(clickedElement.textContent)) {
+      selectedTags = selectedTags;
     } else {
-      console.log('add');
-      selectedTags.push(targetElement.textContent)
+      selectedTags.push(clickedElement.textContent);
     }
-    showSelectedTags(selectedTags)
-    const anouncements = filterJobAnouncements(selectedTags, jobAnouncements)
-    updateUI(anouncements)
-  }
+    showSelectedTags(selectedTags);
+    filteredJobsByTags(selectedTags, jobAnouncements);
 
-})
+  }
+});
 
 function showSelectedTags(selectedTags) {
   headerTags.innerHTML = "";
   if (!selectedTags.length) {
-    return headerContent.classList.remove('show');
+    headerContent.classList.remove("show");
+  } else {
+    headerContent.classList.add("show");
+    selectedTags.forEach((selectedTag) => {
+      const button = document.createElement("button");
+      button.setAttribute("class", "job-card__tag job-card__tag--delete");
+      button.innerHTML = `
+      <span>${selectedTag}</span>
+      <i class="fas fa-times delete"></i>
+      `;
+      headerTags.append(button);
+    });
   }
-  selectedTags.forEach(selectedTag => {
-    const span = document.createElement('span')
-    span.textContent = selectedTag
-    headerTags.append(span)
-  })
-  headerContent.classList.add('show');
+}
+//delete tag function
+
+function deleteTag(e) {
+  const clickedElement = e.target;
+  if (clickedElement.classList.contains('delete')) {
+    selectedTags = selectedTags.filter(selectedTag => selectedTag !== e.target.previousElementSibling.textContent);
+    showSelectedTags(selectedTags)
+    filteredJobsByTags(selectedTags, jobAnouncements)
+  }
 }
 
 
-function filterJobAnouncements(selectedTags, anouncements) {
-  const newAnouncements = []
-
-  // selectegTags
-  // anouncements
-  // anouncement.tags
-
-  selectedTags.forEach(tag => {
-
-  })
 
 
+function filteredJobsByTags(selectedTags, jobAnouncements) {
 
-  anouncements.forEach(anouncement => {
-    anouncement.tags.every((tag)=>{
-      
-    //  return selectedTags.forEach(selectedTag=>{
-    //   return 
-    //   })
-    })
-
-  })
-
-
-
-  console.log(newAnouncements);
-  return newAnouncements
-
+  let filteredAnouncements;
+  filteredAnouncements = jobAnouncements.filter((announcement) =>
+    selectedTags.every((selectedTag) => announcement.tags.includes(selectedTag))
+  );
+  updateUI(filteredAnouncements)
 }
-//clear all
-
-// const clearBtn = document.querySelector('#btn');
-
-// clearBtn.addEventListener('click', function () {
-//   headerTags.textContent = ""
-//   headerContent.classList.remove('show');
-//   updateUI(jobAnouncements)
-// })
-
-// let filteredTag = []
-
-// headerTags.addEventListener('click', function (e) {
-
-//   if (e.target.classList.contains('close')) {
-
-//     e.target.parentElement.remove();
-
-//   }
-//   headerTags.querySelectorAll('.job-card__tag').forEach(tag => {
-
-//     filteredTag = jobAnouncements.filter(x => x.tags.includes(tag.textContent));
-//     updateUI(filteredTag)
-
-//   })
-
-
-// })
 
 
 
-// const movie = [{ title: "The Green Mile" }];
-// movie.push({ title: "Five feet apart" });
 
-// console.log(movie);
+function clearAllTags() {
+  selectedTags = [];
+  showSelectedTags(selectedTags);
+  updateUI(jobAnouncements)
+  headerContent.classList.remove('show')
+}
+function eventHandler() {
+  clearBtn.addEventListener('click', clearAllTags);
+  headerTags.addEventListener('click', deleteTag);
+}
+eventHandler();
 
-// const name = "Leyla";
-// console.log(name.toUpperCase());//LEYLA
-// console.log(name);//leyla
 
-// const a = [];
-// const b = a.push(1, 2)// 
-// console.log(a);//1 2
-
-
-// let firstName = 'book';
-// firstName[0] = 'c';
-// console.log(firstName);
-
-// let book = "The Green Mile";
-// book = "Five feet apart";
+window.addEventListener("DOMContentLoaded", updateUI(jobAnouncements));
